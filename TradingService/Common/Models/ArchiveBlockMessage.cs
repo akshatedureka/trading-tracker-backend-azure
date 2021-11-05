ec2f32a1b0338812bd858a0c3c0496b38601d17a
geodavis <gdavis3737@gmail.com>
2021-11-05 14:32:30 -0600

    Created new function to archive swing block.

---
+++ TradingService/Common/Models/ArchiveBlockMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TradingService.Common.Models
+{
+    public class ArchiveBlockMessage
+    {
+        public string BlockId { get; set; }
+        public string UserId { get; set; }
+        public string Symbol { get; set; }
+        public long NumShares { get; set; }
+        public Guid ExternalBuyOrderId { get; set; }
+        public Guid ExternalSellOrderId { get; set; }
+        public Guid ExternalStopLossOrderId { get; set; }
+        public decimal BuyOrderFilledPrice { get; set; }
+        public DateTime DateBuyOrderFilled { get; set; }
+        public DateTime DateSellOrderFilled { get; set; }
+        public decimal SellOrderFilledPrice { get; set; }
+        public bool IsShort { get; set; }
+    }
+}
